e75f5c4addddd5528d7f7cf2e5449cf1f10a9a2e
Krunal_Rathod <126972198+Krunal6019@users.noreply.github.com>
2025-03-15 21:57:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import openpyxl
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
